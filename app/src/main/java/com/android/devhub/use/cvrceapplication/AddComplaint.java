48a0b68aac516d4cf1e17b5ce5f97369021cefaa
rakeswain <swain.rakesh131@gmail.com>
2018-08-01 09:04:49 +0530

    add complaints

---
+++ app/src/main/java/com/android/devhub/use/cvrceapplication/AddComplaint.java
@@ -0,0 +1,13 @@
+package com.android.devhub.use.cvrceapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_complaint);
+    }
+}
