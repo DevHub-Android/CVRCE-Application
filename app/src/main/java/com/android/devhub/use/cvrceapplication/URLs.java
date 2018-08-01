af8329c5e7229f045e4a58ad705fccda2843a01c
dashsonali <sonali031998@gmail.com>
2018-08-01 14:11:21 +0530

    new

--- app/src/main/java/com/android/devhub/use/cvrceapplication/URLs.java
+++ app/src/main/java/com/android/devhub/use/cvrceapplication/URLs.java
@@ -5,11 +5,10 @@ package com.android.devhub.use.cvrceapplication;
  */
 
 public class URLs {
-<<<<<<< HEAD
+
     public static final String ROOT_URL = "http://192.168.43.62:82/www/public/api.php?apicall=";
-=======
-    public static final String ROOT_URL = "http://192.168.43.226:8080/cvrce/api.php?apicall=";
->>>>>>> bdc51a75611d8487f9ad4dde0491efd5e26f8c47
+//    public static final String ROOT_URL = "http://192.168.43.226:8080/cvrce/api.php?apicall=";
+
     public static final String URL_REGISTER=ROOT_URL+"signup";
     public  static final String URL_LOGIN=ROOT_URL+"login";
     public  static final String URL_COMPLAINT=ROOT_URL+"addComplaint";
