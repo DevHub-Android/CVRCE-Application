c8f75e705b997459468a0188945887431017a72f
aakash1999 <aakash.nanda99@gmail.com>
2018-08-14 13:10:18 +0530

    Swipe Refresh Done!!

--- app/src/main/java/com/android/devhub/use/cvrceapplication/HomeActivity.java
+++ app/src/main/java/com/android/devhub/use/cvrceapplication/HomeActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -57,6 +58,7 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         //Get the data from the intent
         Intent intent = getIntent();
+
 //        if(!SharedPrefManager.getInstance(this).isLoggedIn())
 //        {
 //            finish();
