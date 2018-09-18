0ac3c1ce40b6de23bc5fc6c5a29026e41f4bc40f
aakash1999 <aakash.nanda99@gmail.com>
2018-09-18 21:59:06 +0530

    Principal work done

---
+++ app/src/main/java/com/devhub/use/cvrceapplication/Fragments/PrinciFragmentExamHostel.java
@@ -0,0 +1,160 @@
+package com.devhub.use.cvrceapplication.Fragments;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.devhub.use.cvrceapplication.Adapters.Adapter_Complaints_Authority;
+import com.devhub.use.cvrceapplication.Globals.Globals;
+import com.devhub.use.cvrceapplication.PrincipalHome;
+import com.devhub.use.cvrceapplication.PrincipalHome;
+import com.devhub.use.cvrceapplication.R;
+import com.devhub.use.cvrceapplication.URLs;
+
+import org.json.JSONObject;
+
+public class PrinciFragmentExamHostel extends Fragment {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    JSONObject complaints_data;
+    String serverAddress;
+    Globals global;
+    static RequestQueue myQueue;
+    SwipeRefreshLayout swipeRefreshLayout;
+
+
+    public PrinciFragmentExamHostel() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PrincipalHome activity = (PrincipalHome) getActivity();
+        complaints_data =  activity.getHostelComplains();
+        serverAddress = URLs.SERVER_ADDR;
+        global = (Globals)activity.getApplication();
+        serverAddress = URLs.SERVER_ADDR;
+        myQueue = global.getVolleyQueue();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_complaints, container, false);
+
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.complaint_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        callAdapters();
+
+        swipeRefreshLayout = (SwipeRefreshLayout)view.findViewById(R.id.swipeRefresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Toast.makeText(getContext(), "Hey I am Here", Toast.LENGTH_SHORT).show();
+
+                updateData();
+
+            }
+        });
+
+
+        return view;
+    }
+
+    private void updateData() {
+        class FetchData extends AsyncTask<Void,Void,Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                PrincipalHome activity = (PrincipalHome) getActivity();
+                String url = serverAddress.concat("/public/princi_hostel_complaints.php");
+                JsonObjectRequest request0 = new JsonObjectRequest(Request.Method.GET,url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //Toast.makeText(global, "Student added Successfully", Toast.LENGTH_SHORT).show();
+                        complaints_data = response;
+                        Log.d("Chutiya data dekh", "onResponse: " + response);
+                        //mAdapter.notifyDataSetChanged();
+                        callUpdatedAdapters(complaints_data);
+                        swipeRefreshLayout.setRefreshing(false);
+                        Toast.makeText(getContext(), "Updated", Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("Bhai Bhai",error.toString());
+                        Toast toast = Toast.makeText(getContext(), "Fragements"+error.getMessage(), Toast.LENGTH_LONG);
+                        toast.show();
+                    }
+                });
+                //Add the first request in the queue
+                myQueue.add(request0);
+            }
+        }
+        FetchData fetchData = new FetchData();
+        fetchData.execute();
+    }
+
+    private void callUpdatedAdapters(JSONObject complaints) {
+        // specify an adapter (see also next example)
+        PrincipalHome activity = (PrincipalHome) getActivity();
+        Context context = (PrincipalHome) getContext();
+        complaints_data =  activity.getExamComplains();
+        mAdapter = new Adapter_Complaints_Authority(complaints_data,activity,context,"Principal",
+                activity.getPriority(),activity.getEmp_id(),activity.getFirst_name(),
+                activity.getLast_name());
+        //Log.i("hagga", complaints_data.toString());
+
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void callAdapters(){
+        // specify an adapter (see also next example)
+        PrincipalHome activity = (PrincipalHome) getActivity();
+        Context context = (PrincipalHome) getContext();
+        complaints_data =  activity.getExamComplains();
+        mAdapter = new Adapter_Complaints_Authority(complaints_data,activity,context,"Principal",
+                activity.getPriority(),activity.getEmp_id(),activity.getFirst_name(),
+                activity.getLast_name());
+        //Log.i("hagga", complaints_data.toString());
+
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/devhub/use/cvrceapplication/PrincipalHome.java
@@ -0,0 +1,300 @@
+package com.devhub.use.cvrceapplication;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmentExam;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmentFood;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmentHostel;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmentIndividual;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmentInstitute;
+import com.devhub.use.cvrceapplication.Fragments.MentorFragmetnPlacement;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentExam;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentExamHostel;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentExamIndividual;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentExamInstitute;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentFood;
+import com.devhub.use.cvrceapplication.Fragments.PrinciFragmentPlacement;
+import com.devhub.use.cvrceapplication.Globals.Globals;
+
+import org.json.JSONObject;
+
+public class PrincipalHome extends AppCompatActivity {
+    private JSONObject userComplains,hostelComplains,instiComplains,notificationData,foodComplains,examComplains,placementComplains;
+    String choose,first_name,last_name;
+    String emp_id;
+    int priority;
+    String studRegId;
+
+    EditText studentEditText;
+    static RequestQueue myQueue;
+    Globals global;
+    Context context;
+    String serverAddress;
+    String addUrl;
+    ProgressDialog progressDialog;
+    String name;
+    public JSONObject getUserComplains(){
+        return userComplains;
+    }
+    public JSONObject getHostelComplains(){
+        return hostelComplains;
+    }
+    public JSONObject getInstiComplains(){
+        return instiComplains;
+    }
+    public JSONObject getFoodComplains(){
+        return foodComplains;
+    }
+    public JSONObject getPlacementComplains(){
+        return placementComplains;
+    }
+    public JSONObject getExamComplains(){
+        return examComplains;
+    }
+    public String getFirst_name(){return first_name;}
+    public String getLast_name(){return last_name;}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mentor_home);
+        Intent intent = getIntent();
+        choose = intent.getStringExtra("choose");
+        //mentorId = intent.getStringExtra("mentorId");
+        first_name = intent.getStringExtra("first_name");
+        emp_id = intent.getStringExtra("reg_id");
+        priority = Integer.parseInt(intent.getStringExtra("priority"));
+        last_name = "Principal";
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        context = this;
+        global = (Globals)this.getApplication();
+        serverAddress = URLs.SERVER_ADDR;
+        myQueue = global.getVolleyQueue();
+        Log.e("CHOOSE",choose);
+
+        //addFragment();
+        progressDialog = new ProgressDialog(this);
+
+
+        successCallback();
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public String getEmp_id() {
+        return emp_id;
+    }
+
+    public void successCallback(){
+        class FetchData extends AsyncTask<Void,Void,Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                progressDialog.dismiss();
+                Log.d("inside", "successCallback: " + "inside the method");
+                String url_user_complaints = serverAddress.concat("/public/princi_individual_complaints.php");
+                String url_hostel_complaints = serverAddress.concat("/public/princi_hostel_complaints.php");
+                String url_insti_complaints = serverAddress.concat("/public/princi_institute_complaints.php");
+                String url_exam_complaints = serverAddress.concat("/public/princi_exam_complaints.php");
+                String url_food_complaints = serverAddress.concat("/public/princi_food_complaints.php");
+                String url_placement_complaints = serverAddress.concat("/public/princi_placement_complaints.php");
+                Log.e("HOSTEL LINK MENTOR",url_food_complaints);
+                final JsonObjectRequest request4 = new JsonObjectRequest(Request.Method.GET,url_insti_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        instiComplains = response;
+                        Log.e("institute complaint", "onResponse: " + instiComplains);
+                        addFragment();
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG);
+                        Log.e("ERROR IN MENTOR INSI",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+
+
+
+                final JsonObjectRequest request3 = new JsonObjectRequest(Request.Method.GET,url_hostel_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        hostelComplains = response;
+                        Log.d("hostelcomplaints", "onResponse: " + hostelComplains);
+                        myQueue.add(request4 );
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage(), Toast.LENGTH_LONG);
+                        Log.e("ERROR IN HOSTEL ",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+
+
+                final JsonObjectRequest request2 = new JsonObjectRequest(Request.Method.GET,url_user_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        userComplains = response;
+                        Log.d("usercomplaints", "onResponse: " + userComplains);
+                        myQueue.add(request3 );
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage() , Toast.LENGTH_LONG);
+                        Log.e("ERROR IN MENTOR USER",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+                final JsonObjectRequest request5 = new JsonObjectRequest(Request.Method.GET,url_food_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        foodComplains = response;
+                        //   Log.d("usercomplaints", "onResponse: " + userComplains);
+                        myQueue.add(request2 );
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage() , Toast.LENGTH_LONG);
+                        Log.e("ERROR IN MENTOR FOOD",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+                final JsonObjectRequest request6 = new JsonObjectRequest(Request.Method.GET,url_exam_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        examComplains = response;
+                          Log.e("exam complains",examComplains.toString());
+                        myQueue.add(request5 );
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage() , Toast.LENGTH_LONG);
+                        Log.e("ERROR IN MENTOR EXAM",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+                final JsonObjectRequest request7 = new JsonObjectRequest(Request.Method.GET,url_placement_complaints,null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        placementComplains = response;
+                        //Log.d("usercomplaints", "onResponse: " + userComplains);
+                        myQueue.add(request6);
+
+
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast toast = Toast.makeText(context, error.getMessage() , Toast.LENGTH_LONG);
+                        Log.e("ERROR IN PLCMNT",error.getMessage());
+                        toast.show();
+                    }
+                }) ;
+                myQueue.add(request7);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                progressDialog.setMessage("Loading please wait....");
+                progressDialog.show();
+            }
+        }
+
+        FetchData fetchData = new FetchData();
+        fetchData.execute();
+
+    }
+    private  void addFragment(){
+        FragmentManager fragmentManager
+                = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction
+                = fragmentManager.beginTransaction();
+        if(choose.equals("hostel"))
+        {
+            PrinciFragmentExamHostel fragment = new PrinciFragmentExamHostel();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commit();
+        }else if(choose.equals("food"))
+        {
+            PrinciFragmentFood fragment = new PrinciFragmentFood();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commitAllowingStateLoss();
+        }else if(choose.equals("other"))
+        {
+            PrinciFragmentExamIndividual fragment = new PrinciFragmentExamIndividual();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commitAllowingStateLoss();
+        }
+        else if(choose.equals("exam"))
+        {
+            Toast.makeText(getApplicationContext(), "Going to exam", Toast.LENGTH_SHORT).show();
+            PrinciFragmentExam fragment = new PrinciFragmentExam();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commitAllowingStateLoss();
+        }
+        else if(choose.equals("placement"))
+        {
+            PrinciFragmentPlacement fragment = new PrinciFragmentPlacement();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commitAllowingStateLoss();
+        }else if(choose.equals("dsw"))
+        {
+            PrinciFragmentExamInstitute fragment = new PrinciFragmentExamInstitute();
+            fragmentTransaction.add(R.id.container,fragment);
+            fragmentTransaction.commitAllowingStateLoss();
+        }
+
+
+
+
+
+    }
+}
